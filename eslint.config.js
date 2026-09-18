e76b4071982308b135787d4ba5247f5e49cd5c98
JS MacBook Pro <j_seokpark@naver.com>
2026-09-18 16:31:40 +0900

    chore: Vercel CD 워크플로우 구성

--- eslint.config.js
+++ eslint.config.js
@@ -9,7 +9,7 @@ import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default defineConfig([
-  globalIgnores(['dist', 'coverage']),
+  globalIgnores(['dist', 'coverage', '.vercel']),
   {
     files: ['**/*.{js,mjs,cjs,ts,tsx}'],
     extends: [js.configs.recommended],
